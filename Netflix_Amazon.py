7fc6b989f43c77392ee3cabfa6b4fdb5e4c561f1
Shivamalu-Poojari <113976421+Shivamalu-Poojari@users.noreply.github.com>
2022-09-20 14:37:19 +0530

    Add files via upload

---
+++ Netflix_Amazon.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 13:27:28 2022
+
+@author: Shivamalu Poojari
+"""
+
+!pip install pandas==1.3.3
+!pip install requests==2.26.0
+!mamba install bs4==4.10.0 -y
+!mamba install html5lib==1.1 -y
+!pip install lxml==4.6.4
+!pip install plotly==5.3.1
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/netflix_data_webpage.html"
+
+data  = requests.get(url).text
+
+soup = BeautifulSoup(data, 'html5lib')
+
+netflix_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+
+# First we isolate the body of the table which contains all the information
+# Then we loop through each row and find all the column values for each row
+for row in soup.find("tbody").find_all('tr'):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    adj_close = col[5].text
+    volume = col[6].text
+    
+    # Finally we append the data of each row to the table
+    netflix_data = netflix_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True) 
+    
+    
+netflix_data.head()
+
+read_html_pandas_data = pd.read_html(url)
+
+read_html_pandas_data = pd.read_html(str(soup))
+
+netflix_dataframe = read_html_pandas_data[0]
+
+netflix_dataframe.head()
+
+# =============================================================================
+# amazon_data
+# =============================================================================
+url2 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/amazon_data_webpage.html"
+
+html_data  = requests.get(url2).text
+
+soup = BeautifulSoup(html_data, 'html5lib')
+
+amazon_data = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+
+for row in soup.find("tbody").find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    Open = col[1].text
+    high = col[2].text
+    low = col[3].text
+    close = col[4].text
+    adj_close = col[5].text
+    volume = col[6].text
+    
+    amazon_data = amazon_data.append({"Date":date, "Open":Open, "High":high, "Low":low, "Close":close, "Adj Close":adj_close, "Volume":volume}, ignore_index=True)
+    
+    
+amazon_data.head()
+
+amazon_data.columns
+
+amazon_data.Open.tail(1)
